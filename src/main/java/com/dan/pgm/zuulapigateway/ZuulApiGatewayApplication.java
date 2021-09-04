b78d0e03954386fbda9fb84849e7c0bc315aeefa
micapierotti <micapierotti@live.com.ar>
2021-09-04 13:38:51 -0300

    Agregado de eureka

--- src/main/java/com/dan/pgm/zuulapigateway/ZuulApiGatewayApplication.java
+++ src/main/java/com/dan/pgm/zuulapigateway/ZuulApiGatewayApplication.java
@@ -3,10 +3,12 @@ package com.dan.pgm.zuulapigateway;
 import com.dan.pgm.zuulapigateway.filters.pre.SimpleFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableZuulProxy
 public class ZuulApiGatewayApplication {
 
